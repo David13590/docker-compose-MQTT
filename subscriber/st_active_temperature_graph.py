ae28196d30aa5b6902647da2b1e81a62a0780a2b
DF <DF@VMDebian.com>
2025-05-18 04:35:44 -0500

    temp sensors show highest temp within timeframe

--- subscriber/st_active_temperature_graph.py
+++ subscriber/st_active_temperature_graph.py
@@ -12,32 +12,40 @@ conn = st.connection('temperature_db', type='sql')
 def updateGraph():
 	conn = st.connection('temperature_db', type='sql')
 	
-	#Add sensor query here
-	temperature1_sensor1 = conn.query('SELECT * FROM temperature1 WHERE sensor_name = "DS18B20_1" ORDER BY id')
-	temperature1_sensor1_high = conn.query('SELECT MAX(reading), timestamp FROM temperature1 WHERE sensor_name = "DS18B20_1"')
+	#Query the db
+	#Sensor1
+	temp1_sensor1 = conn.query('SELECT * FROM temperature1 WHERE sensor_name = "DS18B20_1" ORDER BY id')
+	temp1_sensor1_high_timeframe = conn.query('SELECT MAX(reading), timestamp FROM temperature1 WHERE datetime(timestamp) >=datetime((SELECT timestamp FROM temperature1 WHERE sensor_name = "DS18B20_1" ORDER BY rowid desc LIMIT 1), "-7 days") AND sensor_name = "DS18B20_1";')
 	
-	temperature1_sensor2 = conn.query('SELECT * FROM temperature1 WHERE sensor_name = "DS18B20_2" ORDER BY id')
-	temperature1_sensor2_high = conn.query('SELECT MAX(reading), timestamp FROM temperature1 WHERE sensor_name = "DS18B20_2"')
+	#Sensor2
+	temp1_sensor2 = conn.query('SELECT * FROM temperature1 WHERE sensor_name = "DS18B20_2" ORDER BY id')
+	temp1_sensor2_high_timeframe = conn.query('SELECT MAX(reading), timestamp FROM temperature1 WHERE datetime(timestamp) >=datetime((SELECT timestamp FROM temperature1 WHERE sensor_name = "DS18B20_2" ORDER BY rowid desc LIMIT 1), "-7 days") AND sensor_name = "DS18B20_2";')
 	
-	temperature1_sensor3 = conn.query('SELECT * FROM temperature1 WHERE sensor_name = "DS18B20_3" ORDER BY id')
-	temperature1_sensor3_high = conn.query('SELECT MAX(reading), timestamp FROM temperature1 WHERE sensor_name = "DS18B20_3"')
+	#Sensor3
+	temp1_sensor3 = conn.query('SELECT * FROM temperature1 WHERE sensor_name = "DS18B20_3" ORDER BY id')
+	temp1_sensor3_high_timeframe = conn.query('SELECT MAX(reading), timestamp FROM temperature1 WHERE datetime(timestamp) >=datetime((SELECT timestamp FROM temperature1 WHERE sensor_name = "DS18B20_3" ORDER BY rowid desc LIMIT 1), "-7 days") AND sensor_name = "DS18B20_3";')
 	
-	#Create dataframes
-	temperature1_sensor1_df = pd.DataFrame(temperature1_sensor1)
-	temperature1_sensor1_high_df = pd.DataFrame(temperature1_sensor1_high)
-	temperature1_sensor1_high_string = temperature1_sensor1_high_df.to_string(header=False, index=False, index_names=False)
 	
-	temperature1_sensor2_df = pd.DataFrame(temperature1_sensor2)
-	temperature1_sensor2_high_df = pd.DataFrame(temperature1_sensor2_high)
-	temperature1_sensor2_high_string = temperature1_sensor2_high_df.to_string(header=False, index=False, index_names=False)
+	#Create dataframes and format
+	#Sensor1
+	temp1_sensor1_df = pd.DataFrame(temp1_sensor1)
+	temp1_sensor1_high_timeframe_df = pd.DataFrame(temp1_sensor1_high_timeframe)
+	temp1_sensor1_high_timeframe_string = temp1_sensor1_high_timeframe_df.to_string(header=False, index=False, index_names=False)
+	
+	#Sensor2
+	temp1_sensor2_df = pd.DataFrame(temp1_sensor2)	
+	temp1_sensor2_high_timeframe_df = pd.DataFrame(temp1_sensor2_high_timeframe)
+	temp1_sensor2_high_timeframe_string = temp1_sensor2_high_timeframe_df.to_string(header=False, index=False, index_names=False)
+	
 	#temperature1_sensor2_high_df = pd.Series({"DS18B20_2 High":[temperature1_sensor2_high]})
 	
-	temperature1_sensor3_df = pd.DataFrame(temperature1_sensor3)
-	temperature1_sensor3_high_df = pd.DataFrame(temperature1_sensor3_high)
-	temperature1_sensor3_high_string = temperature1_sensor3_high_df.to_string(header=False, index=False, index_names=False)
+	#Sensor3
+	temp1_sensor3_df = pd.DataFrame(temp1_sensor3)
+	temp1_sensor3_high_timeframe_df = pd.DataFrame(temp1_sensor3_high_timeframe)
+	temp1_sensor3_high_timeframe_string = temp1_sensor3_high_timeframe_df.to_string(header=False, index=False, index_names=False)
 	
 	#Append daraframe to return
-	return temperature1_sensor1_df, temperature1_sensor2_df, temperature1_sensor3_df, temperature1_sensor1_high_string, temperature1_sensor2_high_string, temperature1_sensor3_high_string
+	return temp1_sensor1_df, temp1_sensor2_df, temp1_sensor3_df, temp1_sensor1_high_timeframe_string, temp1_sensor2_high_timeframe_string, temp1_sensor3_high_timeframe_string
 
 # Main Streamlit app
 placeholder = st.empty()
