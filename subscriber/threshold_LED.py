fd4872dc0f83f906d4bf9f391f4869b8a9a47a1c
DF <DF@VMDebian.com>
2025-06-01 11:46:40 +0200

    updated threshold_led

--- subscriber/threshold_LED.py
+++ subscriber/threshold_LED.py
@@ -2,15 +2,59 @@ import RPi.GPIO as GPIO
 import time
 import sqlite3 
 
-con = sqlite3.connect("temperature.db")
-cur = con.cursor()
+count = 0
+threshold1 = 25
+threshold2 = 28
+redPin = 17
+yellowPin = 27
+greenPin = 22
 
-sensor1 = cur.execute("SELECT reading FROM temperature1 WHERE sensor_name = "DS18B20_1" ORDER BY rowid desc LIMIT 1")
-print ("Latest sensor1 temp: "+sensor1)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(redPin, GPIO.OUT)
+GPIO.setup(yellowPin, GPIO.OUT)
+GPIO.setup(greenPin, GPIO.OUT)
 
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(4, GPIO.OUT)
+conn = sqlite3.connect("temperature.db")
+cursor = conn.cursor()
+
+def getSensor1Latest():
+    cursor.execute('SELECT reading FROM temperature1 WHERE sensor_name = "DS18B20_1" ORDER BY rowid desc LIMIT 1')
+    sensor1Latest = cursor.fetchall()
+    return sensor1Latest
+
+def getSensor2Latest():
+    cursor.execute('SELECT reading FROM temperature1 WHERE sensor_name = "DS18B20_2" ORDER BY rowid desc LIMIT 1')
+    sensor2Latest = cursor.fetchall()
+    return sensor2Latest
+
+def getSensor3Latest():
+    cursor.execute('SELECT reading FROM temperature1 WHERE sensor_name = "DS18B20_3" ORDER BY rowid desc LIMIT 1')
+    sensor3Latest = cursor.fetchall()
+    return sensor3Latest
+
+sensor1LastTemp = getSensor1Latest()
+sensor2LastTemp = getSensor2Latest()
+sensor3LastTemp = getSensor3Latest()
+
+lastTemp1 = sensor1LastTemp[0][0]
+lastTemp2 = sensor2LastTemp[0][0]
+lastTemp3 = sensor3LastTemp[0][0]
+
+print ("Latest temps: ")
+print (lastTemp1)
+print (lastTemp2)
+print (lastTemp3)
+
+while count < 3:
+    if lastTemp1 < threshold1:
+        print ("Sensor1 temp lower than: 24")
+        GPIO.output(redPin,True)
+        time.sleep(1)
+        GPIO.output(redPin,False)
+        time.sleep(1)
+
+
 
 # state = True
 
